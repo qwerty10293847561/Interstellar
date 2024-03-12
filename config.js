6dd3b24f7826a07a9cf2c1a4a546d31238060cba
qwerty10293847561 <157546533+qwerty10293847561@users.noreply.github.com>
2024-03-12 07:52:10 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  challenge: true,
+  users: {
+    // username: 'password', you can add multiple users.
+    donovan: 'rahhhhh',
+    mason: 'thedividedone',
+    charles: 'ambatublow',
+    adrian: 'RoxyKobe17',
+    kenzo: 'idkbro',
+  },
+}
+export default config
